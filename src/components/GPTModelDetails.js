099edba15d830e5cf09f981a382492c4c771c8a2
GoForge <goforge@example.com>
2024-03-05 18:07:48 +0800

    Add GPTModelDetails component to display detailed information about GPT models

---
+++ src/components/GPTModelDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GPTModelDetails = () => {
+  return (
+    <div>
+      <h1s>GPT Model Details</h1>
+      <p>This component will display detailed information about various GPT models.</p>
+    </div>
+  );
+};
+
+export default GPTModelDetails;
