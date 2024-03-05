ceb1f7b0e75f1cbac93f8824a622154cb2a5079e
GoForgeBot <example@example.com>
2024-03-05 15:37:54 +0800

    Update App.js to import and use gpts.json data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Demo from './pages/Demo';
 import CaseStudies from './pages/CaseStudies';
+import gptsData from './gpts.json'; // Import gpts.json
 import './App.css';
 
 function App() {
@@ -10,14 +11,14 @@ function App() {
     <Router basename="/GoForge">
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home data={gptsData} />} />
           <Route path="/demo" element={<Demo />} />
           <Route path="/case-studies" element={<CaseStudies />} />
         </Routes>
       </div>
     </Router>
 
-  );
+ );
 }
 
 export default App;
