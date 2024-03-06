1dbecc2e06214b2ce0f1a660ffaf3595da7fb09e
GoForge <goforge@example.com>
2024-03-06 08:28:48 +0800

    Apply theme class based on context in App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react';
+
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Demo from './pages/Demo';
 import CaseStudies from './pages/CaseStudies';
 import gptsData from './gpts.json';
 import ThemeSwitchButton from './components/ThemeSwitchButton';
-import { ThemeProvider } from './context/ThemeContext'; // Import ThemeProvider
+import { ThemeContext, ThemeProvider } from './context/ThemeContext'; // Import ThemeContext
 import './App.css';
 
 function App() {
+  const { theme } = useContext(ThemeContext); // Use ThemeContext to get the current theme
+
   return (
     <ThemeProvider>
       <Router basename="/GoForge">
-        <div className="App">
+        <div className={`App ${theme}`}> {/* Apply the theme class here */}
           <Routes>
             <Route path="/" element={<Home data={gptsData} />} />
             <Route path="/demo" element={<Demo />} />
