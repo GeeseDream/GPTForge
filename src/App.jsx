668f327a09245a4027686e168b7eb5efa6da9e45
GoForgeBot <example@example.com>
2024-03-10 23:21:19 +0800

    Correctly update App.jsx to fetch data from a remote URL using fetch API

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import Home from './pages/Home';
 import Demo from './pages/Demo';
 import CaseStudies from './pages/CaseStudies';
-import gptsData from 'https://raw.githubusercontent.com/GeeseDream/goforge-bucket/build/gptlist.json';
 import ThemeSwitchButton from './components/ThemeSwitchButton';
 import { ThemeProvider } from './context/ThemeContext';
 import './App.css';
 
 function App() {
+    const [gptsData, setGptsData] = useState([]);
+
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/GeeseDream/goforge-bucket/build/gptlist.json')
+            .then(response => response.json())
+            .then(data => setGptsData(data))
+            .catch(error => console.error('Error fetching gpts data:', error));
+    }, []);
+
     return (
       <ThemeProvider>
       <Router>
