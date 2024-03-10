bb5b02eb27ccf6d371ea8df743fe9a9a3b51bbd1
Your Name <you@example.com>
2024-03-11 00:24:54 +0800

    改大图标

--- src/components/GPTModels.jsx
+++ src/components/GPTModels.jsx
@@ -7,7 +7,7 @@ const GPTModels = (props) => {
     <div className='gpt-models-container'>
       {props.data.map(model => (
         <div className="gpt-model-card" onClick={() => window.open(model.link, "_blank", "noreferrer,noopener")}>
-          <img src={model.icon} alt="Model Icon" style={{width: '50px', height: '50px'}}/>
+          <img src={model.icon} alt="Model Icon" style={{width: '100px', height: '100px'}}/>
           <h1 style={{ fontSize: '18px' }}>{model.name}</h1>
           <p>{model.description}</p>
         </div>
