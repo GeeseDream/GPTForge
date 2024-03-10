7a3778703c7c2ff26d0dbcd7d79cdee207b87919
Sen Cao <hicaosenq@gmail.com>
2024-03-10 23:52:01 +0800

    使用远程数据

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,12 @@
 
 import React from 'react';
 import GPTModels from '../components/GPTModels';
-import gptsData from '../gpts.json';
 
-const Home = () => (
+const Home = ({ data }) => (
   <div className="home">
     <h1>Welcome to GoForge</h1>
     <p>Explore the capabilities of GPT models and how they can transform your project.</p>
-    <GPTModels data={gptsData} />
+    <GPTModels data={data} />
   </div>
 );
 
